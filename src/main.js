3748145b3522bf4780e517bb757f556d2b8afdec
Mr427 <30674162+Mr427@users.noreply.github.com>
2018-11-24 14:20:57 -0500

    install node-sass
    node-sass install

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ const React = require("react");
 const ReactDom = require("react-dom");
 const App = require("./components/app");
 
+/*
+const styleHeader = document.createElement("link");
+styleHeader.setAttribute("rel", "stylesheet");
+styleHeader.setAttribute("link", "./src/style/css/main-style.css");
+document.head.appendChild(styleHeader);
+*/
+
 const container = document.createElement("div");
+container.setAttribute("id", "root");
 document.body.appendChild(container);
 
 ReactDom.render(<App />, container);
