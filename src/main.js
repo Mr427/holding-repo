48b691c2d16c3b40a5dd53817126312f6779ef59
Mr427 <30674162+Mr427@users.noreply.github.com>
2018-11-28 16:25:02 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ import React from "react";
 import ReactDom from"react-dom";
 import Game from "./components/app";
 import "./style/scss/main-style.scss"
-/*
-const styleHeader = document.createElement("link");
-styleHeader.setAttribute("rel", "stylesheet");
-styleHeader.setAttribute("link", "./src/style/css/main-style.css");
-document.head.appendChild(styleHeader);
-*/
 
 const container = document.createElement("div");
 container.setAttribute("id", "root");
